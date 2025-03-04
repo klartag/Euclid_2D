4a61cf4ffeee03fbff397531b8fb3714aa07b2ab
boaz4 <boaz.gub@gmail.com>
2025-03-04 21:00:39 +0200

    Can now evaluate line_circle and circle_circle intersections, even if none of the intersection points are already known

--- rules/embeddings/constructions/circle_intersection.py
+++ rules/embeddings/constructions/circle_intersection.py
@@ -1,7 +1,11 @@
+from typing import Tuple
+from mpmath import mpf
+
 from ..undefined_embedding_error import UndefinedEmbeddingError
-from ..embedded_objects import EmbeddedPoint, EmbeddedLine, EmbeddedCircle
+from ..embedded_objects import EmbeddedPoint, EmbeddedLine, EmbeddedCircle, EPSILON
 
 from .reflection import reflect_point
+from .radical_axis import radical_axis
 
 
 def line_circle_other_intersection(
@@ -25,3 +29,27 @@ def circle_circle_other_intersection(
     if other_intersection.is_equal(first_intersection):
         raise UndefinedEmbeddingError("The circles are tangent and do not have another intersection.")
     return other_intersection
+
+
+def quadratic_equation_solutions(a: mpf, b: mpf, c: mpf) -> Tuple[mpf, ...]:
+    determinant = b**2 - 4 * a * c
+    if abs(determinant) < EPSILON ** 2:
+        return (-b / 2*a,)
+    elif determinant < 0:
+        raise UndefinedEmbeddingError("No solution was found for the quadratic equation.")
+    else:
+        return ((-b + determinant**0.5) / (2 * a), (-b - determinant**0.5) / (2 * a))
+
+
+def line_circle_intersection(line: EmbeddedLine, circle: EmbeddedCircle) -> Tuple[EmbeddedPoint, ...]:
+    a = line.direction.length_squared()
+    b = 2 * line.direction.scalar_product(line.point - circle.center)
+    c = (line.point - circle.center).length_squared() - circle.radius_squared
+
+    time_options = quadratic_equation_solutions(a, b, c)
+    return tuple([line.point + line.direction.scale(t) for t in time_options])
+
+
+def circle_circle_intersection(circle0: EmbeddedCircle, circle1: EmbeddedCircle) -> Tuple[EmbeddedPoint, ...]:
+    axis = radical_axis(circle0, circle1)
+    return line_circle_intersection(axis, circle0)
--- rules/embeddings/method_dictionaries.py
+++ rules/embeddings/method_dictionaries.py
@@ -3,7 +3,7 @@ from typing import Dict
 from .constructions.angle_bisectors import internal_angle_bisector, external_angle_bisector
 from .constructions.center import center
 from .constructions.circle import circumcenter, circumcircle, circle_from_center_and_point, circle_from_center_and_radius, circle_from_two_points_and_angle
-from .constructions.circle_intersection import line_circle_other_intersection, circle_circle_other_intersection
+from .constructions.circle_intersection import line_circle_other_intersection, circle_circle_other_intersection, line_circle_intersection, circle_circle_intersection
 from .constructions.incenters import incenter, excenter, incircle, excircle
 from .constructions.isogonal_conjugate import isogonal_conjugate
 from .constructions.line import line
@@ -33,51 +33,61 @@ from .predicates.quadrilaterals import trapezoid, isosceles_trapezoid, parallelo
 from .predicates.tangent import tangent
 from .predicates.triangles import isosceles_triangle, congruent_triangles, anti_congruent_triangles, similar_triangles, anti_similar_triangles
 
-from .types import ConstructionMethod, PredicateMethod, normalize_return_type
+from .types import ConstructionMethod, ExtendedConstructionMethod, PredicateMethod, normalize_return_type
 
 
-CONSTRUCTION_METHOD_DICTIONARY: Dict[str, ConstructionMethod] = {
-    name: normalize_return_type(func) for name, func in {
-        'altitude': altitude,
-        'angle': angle,
-        'center': center,
-        'centroid': centroid,
-        'circle_circle_other_intersection': circle_circle_other_intersection,
-        'Circle': circumcircle,
-        'circle_from_center_and_point': circle_from_center_and_point,
-        'circle_from_center_and_radius': circle_from_center_and_radius,
-        'circle_from_two_points_and_angle': circle_from_two_points_and_angle,
-        'circumcenter': circumcenter,
-        'direction': direction,
-        'distance': distance,
-        'excenter': excenter,
-        'excircle': excircle,
-        'external_angle_bisector': external_angle_bisector,
-        'incenter': incenter,
-        'incircle': incircle,
-        'internal_angle_bisector': internal_angle_bisector,
-        'isogonal_conjugate': isogonal_conjugate,
-        'line_circle_other_intersection': line_circle_other_intersection,
-        'line_circle_tangent_point': tangent_point,
-        'line_intersection': line_intersection,
-        'Line': line,
-        'log': log,
-        'median': median,
-        'midpoint': midpoint,
-        'nine_point_circle': nine_point_circle,
-        'orientation': orientation,
-        'orthocenter': orthocenter,
-        'parallel_line': parallel_line,
-        'perpendicular_bisector': perpendicular_bisector,
-        'perpendicular_line': perpendicular_line,
-        'point_circle_tangent_line': tangent_line,
-        'power_of_a_point': power_of_a_point,
-        'projection': project,
-        'radical_axis': radical_axis,
-        'radius': radius,
-        'rotate_point': rotate_point,
-    }.items()
+_SIMPLE_CONSTRUCTION_METHOD_DICTIONARY: Dict[str, ExtendedConstructionMethod] = {
+    'altitude': altitude,
+    'angle': angle,
+    'center': center,
+    'centroid': centroid,
+    'circle_circle_other_intersection': circle_circle_other_intersection,
+    'Circle': circumcircle,
+    'circle_from_center_and_point': circle_from_center_and_point,
+    'circle_from_center_and_radius': circle_from_center_and_radius,
+    'circle_from_two_points_and_angle': circle_from_two_points_and_angle,
+    'circumcenter': circumcenter,
+    'direction': direction,
+    'distance': distance,
+    'excenter': excenter,
+    'excircle': excircle,
+    'external_angle_bisector': external_angle_bisector,
+    'incenter': incenter,
+    'incircle': incircle,
+    'internal_angle_bisector': internal_angle_bisector,
+    'isogonal_conjugate': isogonal_conjugate,
+    'line_circle_other_intersection': line_circle_other_intersection,
+    'line_circle_tangent_point': tangent_point,
+    'line_intersection': line_intersection,
+    'Line': line,
+    'log': log,
+    'median': median,
+    'midpoint': midpoint,
+    'nine_point_circle': nine_point_circle,
+    'orientation': orientation,
+    'orthocenter': orthocenter,
+    'parallel_line': parallel_line,
+    'perpendicular_bisector': perpendicular_bisector,
+    'perpendicular_line': perpendicular_line,
+    'point_circle_tangent_line': tangent_line,
+    'power_of_a_point': power_of_a_point,
+    'projection': project,
+    'radical_axis': radical_axis,
+    'radius': radius,
+    'rotate_point': rotate_point,
 }
+
+
+CONSTRUCTION_METHOD_DICTIONARY: Dict[str, ConstructionMethod]  = {
+    'line_circle_intersection': line_circle_intersection,
+    'circle_circle_intersection': circle_circle_intersection,
+}
+
+CONSTRUCTION_METHOD_DICTIONARY.update({
+    name: normalize_return_type(func)
+    for name, func in _SIMPLE_CONSTRUCTION_METHOD_DICTIONARY.items()
+})
+
 
 PREDICATE_METHOD_DICTIONARY: Dict[str, PredicateMethod] = {
     'anti_congruent_triangles': anti_congruent_triangles,
