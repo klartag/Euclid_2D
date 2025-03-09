3c0ed46416c558d26f9061bfe19a98f99924a90d
boaz4 <boaz.gub@gmail.com>
2025-03-09 18:50:22 +0200

    Moved three more constructions out of the constructions_database

--- rules/embeddings/embedder/embedded_geo_objects/method_dictionary.py
+++ rules/embeddings/embedder/embedded_geo_objects/method_dictionary.py
@@ -3,16 +3,48 @@ from typing import Dict
 
 from ....rule_utils import POINT, LINE, CIRCLE
 
-from ...types import ConstructionMethod, ExtendedConstructionMethod, normalize_return_type
+from ...types import ConstructionMethod, normalize_return_type
 from ...constructions.circle_intersection import line_circle_intersection, circle_circle_intersection
+from ...constructions.circle import circle_from_center_and_point, circle_from_center_and_radius, circle_from_two_points_and_angle
+
 
 @dataclass
 class ConstructionData:
     type: str
     method: ConstructionMethod
+    
+    def __post_init__(self):
+        self.method = normalize_return_type(self.method)
 
 
-EMBEDDED_GEO_OBJECT_CONSTRUCTION_DICTIONARY: Dict[str, ConstructionData] = {
-    'line_circle_intersection': (POINT, line_circle_intersection),
-    'circle_circle_intersection': (POINT, circle_circle_intersection),
+_EMBEDDED_GEO_OBJECT_POINT_CONSTRUCTIONS: Dict[str, ConstructionMethod] = {
+    'line_circle_intersection': line_circle_intersection,
+    'circle_circle_intersection': circle_circle_intersection,
 }
+
+_EMBEDDED_GEO_OBJECT_LINE_CONSTRUCTIONS: Dict[str, ConstructionMethod] = {
+    
+}
+
+_EMBEDDED_GEO_OBJECT_CIRCLE_CONSTRUCTIONS: Dict[str, ConstructionMethod] = {
+    'circle_from_center_and_point': circle_from_center_and_point,
+    'circle_from_center_and_radius': circle_from_center_and_radius,
+    'circle_from_two_points_and_angle': circle_from_two_points_and_angle,
+}
+
+EMBEDDED_GEO_OBJECT_CONSTRUCTION_DICTIONARY: Dict[str, ConstructionData] = { }
+
+EMBEDDED_GEO_OBJECT_CONSTRUCTION_DICTIONARY.update([
+    (name, ConstructionData(POINT, method))
+    for (name, method) in _EMBEDDED_GEO_OBJECT_POINT_CONSTRUCTIONS.items()
+])
+
+EMBEDDED_GEO_OBJECT_CONSTRUCTION_DICTIONARY.update([
+    (name, ConstructionData(LINE, method))
+    for (name, method) in _EMBEDDED_GEO_OBJECT_LINE_CONSTRUCTIONS.items()
+])
+
+EMBEDDED_GEO_OBJECT_CONSTRUCTION_DICTIONARY.update([
+    (name, ConstructionData(CIRCLE, method))
+    for (name, method) in _EMBEDDED_GEO_OBJECT_CIRCLE_CONSTRUCTIONS.items()
+])
