31152d64cc75f9255f66da75596ee03afcfa5300
boaz4 <boaz.gub@gmail.com>
2025-03-09 22:07:41 +0200

    Fixed bug causing new points to not be distributed as expected

--- rules/embeddings/constructions/new_object.py
+++ rules/embeddings/constructions/new_object.py
@@ -8,7 +8,7 @@ from ..embedded_objects import EmbeddedPoint, EmbeddedLine, EmbeddedCircle, PI
 def new_point() -> EmbeddedPoint:
     radius = 10 * uniform(0, 1) ** 0.5
     angle = 2 * PI * uniform(0, 1)
-    return EmbeddedPoint(mpf(cos(radius * angle)), mpf(sin(radius * angle)))
+    return EmbeddedPoint(mpf(radius * cos(angle)), mpf(radius * sin(angle)))
 
 def new_line() -> EmbeddedLine:
     return EmbeddedLine(new_point(), new_point())
