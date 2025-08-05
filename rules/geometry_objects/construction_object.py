a4b6c43ba721a2697bc5409d6d4c473b3dbb0c8d
boaz4 <boaz.gub@gmail.com>
2025-08-05 18:31:12 +0300

    Logs work now + matched test set to match existing proofs

--- rules/geometry_objects/construction_object.py
+++ rules/geometry_objects/construction_object.py
@@ -9,8 +9,6 @@ from ..errors import ProofCheckError, GeometryError
 from .atom import Atom
 from .geo_type import GeoType
 from .geo_object import GeoObject
-from .literal import ONE, ZERO
-from .equation_object import EquationObject
 
 if TYPE_CHECKING:
     from ..predicates.predicate import Predicate
@@ -154,37 +152,3 @@ class ConstructionObject(GeoObject):
 
     def involved_objects(self) -> set[GeoObject]:
         return {self} | union(comp.involved_objects() for comp in self.components)
-
-
-@dataclass(frozen=True)
-class LogConstruction(Construction):
-    """
-    Constructs the log of an object.
-    When taking the log of an equation, we might have log(2*X), which should be translated to log(X) + log(2),
-    or an equation which is a scalar.
-    """
-
-    def __call__(self, *args) -> GeoObject:
-        assert len(args) == 1, f'log_construction called with args {args}!'
-
-        inp = args[0]
-        if isinstance(inp, EquationObject):
-            inp_log_factors = inp.as_log_equation()
-            if inp_log_factors is not None:
-                return sum((obj * factor for obj, factor in inp_log_factors.items()), ZERO)
-
-        # The special conversion failed.
-        return super().__call__(*args)
-
-
-def as_log_equation(self) -> dict[GeoObject, Fraction] | None:
-    if (val := self.as_literal()) is not None:
-        if val <= 0:
-            return None
-        return {ConstructionObject.from_args('log', (self,)): 1}
-    if self.type == GeoType.SCALAR:
-        return {ConstructionObject.from_args('log', (self,)): 1}
-    return None
-
-
-GeoObject.as_log_equation = as_log_equation
