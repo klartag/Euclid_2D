314fa83c63839738155a761c0115b970b237000b
boaz4 <boaz.gub@gmail.com>
2025-10-21 14:34:59 +0300

    Documented equation types + made functional equations work with multiple parameters

--- rules/geometry_trackers/congruence_closure_tracker/input_equations/functional_equation.py
+++ rules/geometry_trackers/congruence_closure_tracker/input_equations/functional_equation.py
@@ -4,7 +4,14 @@ from .abstract_input_equation import AbstractInputEquation
 
 
 @dataclass
-class FunctionalEquation[T](AbstractInputEquation[T]):
-    left0: T
-    left1: T
+class FunctionalEquation[str, T](AbstractInputEquation[T]):
+    '''
+    Represents an equation of the form `F(a_1, a_2, ..., a_n) = b`.
+    Where `F` is held in `function`,
+    `a_1, a_2, ..., a_n` is held in `left`,
+    and `b` is held in `right`.
+    '''
+
+    function: str
+    left: list[T]
     right: T
--- rules/geometry_trackers/congruence_closure_tracker/input_equations/simple_equation.py
+++ rules/geometry_trackers/congruence_closure_tracker/input_equations/simple_equation.py
@@ -5,5 +5,11 @@ from .abstract_input_equation import AbstractInputEquation
 
 @dataclass
 class SimpleEquation[T](AbstractInputEquation[T]):
+    '''
+    Represents an equation of the form `a = b`,
+    where `a` is held in `left`,
+    and `b` is held in `right`.
+    '''
+
     left: T
     right: T
