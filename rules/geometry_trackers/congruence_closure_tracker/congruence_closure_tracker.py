caf1c7b758fcebd3333e423d85701af94d63a3d9
boaz4 <boaz.gub@gmail.com>
2025-10-21 14:54:57 +0300

    Added skeleton for methods in the congruence closure tracker

--- rules/geometry_trackers/congruence_closure_tracker/congruence_closure_tracker.py
+++ rules/geometry_trackers/congruence_closure_tracker/congruence_closure_tracker.py
@@ -7,3 +7,31 @@ class CongruenceClosureTracker[T]:
     class_lists: dict[T, list[T]]
     use_lists: dict[T, list[FunctionalEquation[T]]]
     lookup_table: dict[tuple[T, T], FunctionalEquation[T]]
+
+    def __init__(self):
+        self.pending = []
+        self.representatives = {}
+        self.class_lists = {}
+        self.use_lists = {}
+        self.lookup_table = {}
+        
+    def merge(self, equation: SimpleEquation[T]):
+        raise NotImplementedError()
+    
+    def are_congruent(self, left: T, right: T) -> bool:
+        raise NotImplementedError()
+    
+    def propogate(self):
+        raise NotImplementedError()
+    
+    def normalize(self) -> T:
+        raise NotImplementedError()
+    
+    def explain(self, left: T, right: T):
+        raise NotImplementedError()
+    
+    def explain_along_path(self, left: T, right: T):
+        raise NotImplementedError()
+    
+    
+    
