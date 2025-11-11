c978d6116cac9a0dbe2d784ad4c2b6602848a101
boaz4 <boaz.gub@gmail.com>
2025-11-11 17:33:37 +0200

    Added a congruence closure tracker that tracks text functions (for debug purposes)

---
+++ rules/geometry_trackers/congruence_closure_tracker/text_congruence_closure_tracker.py
@@ -0,0 +1,18 @@
+from ...expression_parse_utils import split_args
+
+from .abstract_congruence_closure_tracker import AbstractCongruenceClosureTracker
+from .terms.generic_function_term import GenericFunctionTerm
+
+
+class TextCongruenceClosureTracker(AbstractCongruenceClosureTracker[str, str]):
+    def deconstruct(self, value: str) -> str | GenericFunctionTerm[str]:
+        assert value.count('(') == value.count(')')
+        if '(' not in value:
+            return value
+        assert value.endswith(')')
+        position = value.index('(')
+        function = value[:position]
+        unparsed_parameters = split_args(value[position + 1:-1])
+        assert unparsed_parameters is not None
+        parsed_parameters = [self.deconstruct(parameter) for parameter in unparsed_parameters]
+        return GenericFunctionTerm(function, tuple(parsed_parameters))
